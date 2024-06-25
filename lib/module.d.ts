8a0f5b70ae3b93d7cb4e41627142454803b98da1
Nadar <git@nadar.io>
2024-06-25 18:56:06 +0000

    chore: Update Node.js version in tests.yml and use npm ci for installing dependencies

--- lib/module.d.ts
+++ lib/module.d.ts
@@ -1,21 +1,5 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
-name: Lint Code
 
-on: [push, pull_request]
-
-jobs:
-  eslint:
-    runs-on: ubuntu-latest
-    steps:
-    - uses: actions/checkout@v2
-    - name: Setup Node.js
-      uses: actions/setup-node@v2
-      with:
-        node-version: '14'
-    - name: Install Dependencies
-      run: npm install
-    - name: Run ESLint
-      run: npx eslint .
 declare module "virtual:*" {
   const component: any;
   export default component;
