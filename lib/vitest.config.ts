86406f37649cd07f6299e17a4ff6b77054e88511
Basil Suter <git@nadar.io>
2026-02-04 12:49:57 +0000

    add resolve alias for @flyo/nitro-astro in vitest config

--- lib/vitest.config.ts
+++ lib/vitest.config.ts
@@ -1,8 +1,14 @@
 /// <reference types="vitest" />
 import { getViteConfig } from 'astro/config';
+import { fileURLToPath } from 'node:url';
 
 export default getViteConfig({
   test: {
     environment: 'node',
+  },
+  resolve: {
+    alias: {
+      '@flyo/nitro-astro': fileURLToPath(new URL('./index.ts', import.meta.url)),
+    },
   },
 });
